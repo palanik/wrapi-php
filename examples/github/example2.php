84ac094c04e2620e087fa23b352f90838e18cda6
palanik <palani.kumanan@gmail.com>
2016-01-02 00:55:53 -0500

    switching to magic method for example

--- examples/github/example2.php
+++ examples/github/example2.php
@@ -6,7 +6,7 @@ require "src/wrapi.php";
 
 $client = new wrapi\wrapi('https://api.github.com/');
 
-$client->register("zen", array("method" => "GET", "path" => "zen"));
+$client("zen", array("method" => "GET", "path" => "zen"));
 
 $resp = $client->zen();
 echo "Today's Zen advice: '". $resp. "'\n\n";
